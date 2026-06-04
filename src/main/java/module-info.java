474c947fbc9092211604ccc96b343cf10e50f6a2
Momotaj Akther <momotaj.akther.happy@gmail.com>
2026-06-04 17:23:44 +0600

    opened modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
-module com.example.cse_213_simulating_dohs_group_31_summer_2026 {
+open module com.example.cse_213_simulating_dohs_group_31_summer_2026 {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.example.cse_213_simulating_dohs_group_31_summer_2026 to javafx.fxml;
     exports com.example.cse_213_simulating_dohs_group_31_summer_2026;
 }
