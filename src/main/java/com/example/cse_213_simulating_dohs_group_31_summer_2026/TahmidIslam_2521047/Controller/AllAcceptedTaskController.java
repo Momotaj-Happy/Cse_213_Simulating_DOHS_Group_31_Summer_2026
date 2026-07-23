f9ba77a30e54ae9ccaaf4c673dcc861f3aa8f84b
tns2241 <tahmidislam5002@gmail.com>
2026-07-23 20:57:19 +0600

    Update 4

---
+++ src/main/java/com/example/cse_213_simulating_dohs_group_31_summer_2026/TahmidIslam_2521047/Controller/AllAcceptedTaskController.java
@@ -0,0 +1,27 @@
+package com.example.cse_213_simulating_dohs_group_31_summer_2026.TahmidIslam_2521047.Controller;
+
+public class AllAcceptedTaskController
+{
+    @javafx.fxml.FXML
+    private TableColumn residentNameTableCol;
+    @javafx.fxml.FXML
+    private TableColumn isCompletedTableCol;
+    @javafx.fxml.FXML
+    private TableView allAcceptedTaskTableView;
+    @javafx.fxml.FXML
+    private TableColumn addressTableCol;
+    @javafx.fxml.FXML
+    private TableColumn taskGivenDateTableCol;
+    @javafx.fxml.FXML
+    private TableColumn residentIdTaleCol;
+    @javafx.fxml.FXML
+    private TableColumn typeTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/cse_213_simulating_dohs_group_31_summer_2026/TahmidIslam_2521047/Controller/AllAssignedTaskController.java
@@ -0,0 +1,29 @@
+package com.example.cse_213_simulating_dohs_group_31_summer_2026.TahmidIslam_2521047.Controller;
+
+public class AllAssignedTaskController
+{
+    @javafx.fxml.FXML
+    private TableColumn residentNameTableCol;
+    @javafx.fxml.FXML
+    private TableView allAssignedTaskTableView;
+    @javafx.fxml.FXML
+    private TableColumn addressTableCol;
+    @javafx.fxml.FXML
+    private TableColumn taskGivenDateTableCol;
+    @javafx.fxml.FXML
+    private TableColumn maintenanceTypeTableCol;
+    @javafx.fxml.FXML
+    private TableColumn residentIdTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acceptSelectedTaskOnAction(ActionEvent actionEvent) {
+    }
+}
